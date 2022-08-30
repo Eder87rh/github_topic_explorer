08f3d2ed46111810cd287e0191ace828c392d170
Eder Ramirez <eder.ramirez87@gmail.com>
2022-08-29 20:25:03 -0500

    tests using next/router fixed

--- pages/topic/topic.test.jsx
+++ pages/topic/topic.test.jsx
@@ -1,44 +1,63 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { MockedProvider } from "@apollo/client/testing";
 import Topic from "./[id]";
-import * as nextRouter from "next/router";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 import { mocks } from "../../graphql/queries/topic";
+import { createMockRouter } from "../../utils/test-utils/createMockRouter";
+// import Router from "next/router";
 
 describe("Topic", () => {
-  beforeAll(() => {
-    nextRouter.useRouter = jest.fn();
-    nextRouter.useRouter.mockImplementation(() => ({
-      route: "/",
-      query: { id: "react" },
-    }));
-  });
-
-  afterEach(() => {
-    nextRouter.useRouter.mockReset();
-  });
-
   it("should render the component", async () => {
+    const router = createMockRouter({
+      query: {
+        id: "react",
+      },
+    });
     render(
-      <MockedProvider mocks={mocks} addTypename={false}>
-        <Topic />
-      </MockedProvider>
+      <RouterContext.Provider value={router}>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Topic />
+        </MockedProvider>
+      </RouterContext.Provider>
     );
     expect(await screen.findByText("REACT")).toBeInTheDocument();
   });
 
   it("should render angular data", async () => {
-    nextRouter.useRouter.mockImplementation(() => ({
-      route: "/",
-      query: { id: "angular" },
-    }));
-
+    const router = createMockRouter({
+      query: {
+        id: "angular",
+      },
+    });
     render(
-      <MockedProvider mocks={mocks} addTypename={false}>
-        <Topic />
-      </MockedProvider>
+      <RouterContext.Provider value={router}>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Topic />
+        </MockedProvider>
+      </RouterContext.Provider>
     );
     expect(await screen.findByText("ANGULAR")).toBeInTheDocument();
+  });
+
+  it("should redirect to the selected related topic", async () => {
+    const router = createMockRouter({
+      query: {
+        id: "react",
+      },
+    });
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Topic />
+        </MockedProvider>
+      </RouterContext.Provider>
+    );
+
+    const angularButton = await component.findByText("angular");
+    fireEvent.click(angularButton);
+
+    expect(router.push).toHaveBeenCalledTimes(1);
   });
 });
